9e4ac5abccd04990c567553002f85f6226290f83
krins <krinskumar@gmail.com>
2024-12-31 21:54:31 -0500

    snowflake dropped

---
+++ transcribe/test.py
@@ -0,0 +1,7 @@
+from browser_transcribe import browser_transcribe
+
+# renders the browser_transcribe component
+transcribe = browser_transcribe()
+
+# displays the transcribed text
+transcribe
---
+++ transcribe/transcribe.py
@@ -0,0 +1,5 @@
+from browser_transcribe import browser_transcribe
+
+# renders the browser_transcribe component
+def transcribe():
+    return browser_transcribe()
